fd80fdb23a2381e58355f3763b0a7f3a8bd0ff12
abhijitSahaGit <50227833+abhijitSahaGit@users.noreply.github.com>
2019-05-03 14:34:00 +0530

    Delete wordCount.py.py

--- wordCount.py.py
+++
@@ -1,7 +0,0 @@
-import re
-test_string="Modi, is Indian PM @# from BJP.!!!"
-print("test_string="+test_string)
-words=re.findall(r'\w+', test_string)
-print("words="+str(words))
-count=len(words)
-print("Count of words ="+str(count))
